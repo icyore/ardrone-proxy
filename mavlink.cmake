b2daca856c3ddb1dfb3d35ac7fba1cddddc14f1c
v01d <phreakuencies@gmail.com>
2013-04-05 11:45:44 +0200

    fixed cmake for external usage

--- mavlink.cmake
+++ mavlink.cmake
@@ -1,9 +1,9 @@
 set(MAVGEN /usr/share/pyshared/pymavlink/generator/mavgen.py)
 find_package(PythonInterp 2 REQUIRED)
 ADD_CUSTOM_COMMAND(
-   OUTPUT ${PROJECT_SOURCE_DIR}/mavlink/ardrone/mavlink.h
+   OUTPUT ${CMAKE_CURRENT_LIST_DIR}/mavlink/ardrone/mavlink.h
    COMMAND ${PYTHON_EXECUTABLE} ${MAVGEN} --lang=C --output=mavlink ardrone.xml --wire-protocol=1.0
-   DEPENDS ${PROJECT_SOURCE_DIR}/ardrone.xml
-   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+   DEPENDS ${CMAKE_CURRENT_LIST_DIR}/ardrone.xml
+   WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
 )
-add_custom_target(mavlink_headers ALL DEPENDS mavlink/ardrone/mavlink.h)
+add_custom_target(mavlink_headers ALL DEPENDS ${CMAKE_CURRENT_LIST_DIR}/mavlink/ardrone/mavlink.h)
